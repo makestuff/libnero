a325f6d63701bd93638610e557eba18defd6d4f2
Chris McClelland <github.com@m3.ath.cx>
2011-05-22 13:44:01 +0100

    Initial release

---
+++ libnero.c
@@ -0,0 +1,369 @@
+/* 
+ * Copyright (C) 2011 Chris McClelland
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *  
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <makestuff.h>
+#include <libusbwrap.h>
+#include <usb.h>
+#include <liberror.h>
+#include <vendorCommands.h>
+#include "libnero.h"
+
+// -------------------------------------------------------------------------------------------------
+// Declaration of private types & functions
+// -------------------------------------------------------------------------------------------------
+
+typedef enum {
+	SEND_ZEROS,
+	SEND_ONES,
+	SEND_DATA,
+	SEND_MASK
+} SendType;
+
+enum {
+	IS_RESPONSE_NEEDED = 0,
+	IS_LAST = 1,
+	SEND_TYPE = 2
+};
+
+static NeroStatus beginShift(
+	struct NeroHandle *handle, uint32 numBits, SendType sendType, bool isLast,
+	bool isResponseNeeded, const char **error
+) WARN_UNUSED_RESULT;
+
+static NeroStatus doSend(
+	struct NeroHandle *handle, const uint8 *sendPtr, uint16 chunkSize, const char **error
+) WARN_UNUSED_RESULT;
+
+static NeroStatus doReceive(
+	struct NeroHandle *handle, uint8 *receivePtr, uint16 chunkSize, const char **error
+) WARN_UNUSED_RESULT;
+
+static NeroStatus setEndpointSize(
+	struct NeroHandle *handle, const char **error
+) WARN_UNUSED_RESULT;
+
+static NeroStatus setJtagMode(
+	struct NeroHandle *handle, bool enable, const char **error
+) WARN_UNUSED_RESULT;
+
+// -------------------------------------------------------------------------------------------------
+// Public functions
+// -------------------------------------------------------------------------------------------------
+
+// Find the NeroJTAG device, open it.
+//
+NeroStatus neroInitialise(
+	struct usb_dev_handle *device, struct NeroHandle *handle, const char **error)
+{
+	NeroStatus returnCode, nStatus;
+	handle->device = device;
+	nStatus = setEndpointSize(handle, error);
+	CHECK_STATUS(nStatus, "neroInitialise()", NERO_ENDPOINTS);
+	nStatus = setJtagMode(handle, true, error);
+	CHECK_STATUS(nStatus, "neroInitialise()", NERO_ENABLE);
+	return NERO_SUCCESS;
+cleanup:
+	handle->device = NULL;
+	handle->endpointSize = 0;
+	return returnCode;
+}
+
+// Close the cable...drop the USB connection.
+//
+NeroStatus neroClose(struct NeroHandle *handle, const char **error) {
+	NeroStatus returnCode, nStatus;
+	if ( handle->device ) {
+		nStatus = setJtagMode(handle, false, error);
+		CHECK_STATUS(nStatus, "neroClose()", NERO_ENABLE);
+	}
+	returnCode = NERO_SUCCESS;
+cleanup:
+	handle->device = NULL;
+	handle->endpointSize = 0;
+	return returnCode;
+}
+
+// Shift data into and out of JTAG chain.
+//   In pointer may be ZEROS (shift in zeros) or ONES (shift in ones).
+//   Out pointer may be NULL (not interested in data shifted out of the chain).
+//
+NeroStatus neroShift(
+	struct NeroHandle *handle, uint32 numBits, const uint8 *inData, uint8 *outData, bool isLast,
+	const char **error)
+{
+	NeroStatus returnCode, nStatus;
+	uint32 numBytes;
+	uint16 chunkSize;
+	SendType sendType;
+	bool isResponseNeeded;
+
+	if ( inData == ZEROS ) {
+		sendType = SEND_ZEROS;
+	} else if ( inData == ONES ) {
+		sendType = SEND_ONES;
+	} else {
+		sendType = SEND_DATA;
+	}
+	if ( outData ) {
+		isResponseNeeded = true;
+	} else {
+		isResponseNeeded = false;
+	}
+	nStatus = beginShift(handle, numBits, sendType, isLast, isResponseNeeded, error);
+	CHECK_STATUS(nStatus, "neroShift()", NERO_BEGIN_SHIFT);
+	numBytes = bitsToBytes(numBits);
+	while ( numBytes ) {
+		chunkSize = (numBytes>=handle->endpointSize) ? handle->endpointSize : (uint16)numBytes;
+		if ( sendType == SEND_DATA ) {
+			nStatus = doSend(handle, inData, chunkSize, error);
+			CHECK_STATUS(nStatus, "neroShift()", NERO_SEND);
+			inData += chunkSize;
+		}
+		if ( isResponseNeeded ) {
+			nStatus = doReceive(handle, outData, chunkSize, error);
+			CHECK_STATUS(nStatus, "neroShift()", NERO_RECEIVE);
+			outData += chunkSize;
+		}
+		numBytes -= chunkSize;
+	}
+	return NERO_SUCCESS;
+cleanup:
+	return returnCode;
+}
+
+// Apply the supplied bit pattern to TMS, to move the TAP to a specific state.
+//
+NeroStatus neroClockFSM(
+	struct NeroHandle *handle, uint32 bitPattern, uint8 transitionCount, const char **error)
+{
+	NeroStatus returnCode;
+	int uStatus = usb_control_msg(
+		handle->device,
+		USB_ENDPOINT_OUT | USB_TYPE_VENDOR | USB_RECIP_DEVICE,
+		CMD_JTAG_CLOCK_FSM,       // bRequest
+		(uint16)transitionCount,  // wValue
+		0x0000,                   // wIndex
+		(char*)&bitPattern,
+		4,                        // wLength
+		5000                      // timeout (ms)
+	);
+	if ( uStatus < 0 ) {
+		errRender(error, "neroClockFSM(): %s", usb_strerror());
+		FAIL(NERO_CLOCKFSM);
+	}
+	return NERO_SUCCESS;
+cleanup:
+	return returnCode;
+}
+
+// Cycle the TCK line for the given number of times.
+//
+NeroStatus neroClocks(struct NeroHandle *handle, uint32 numClocks, const char **error) {
+	NeroStatus returnCode;
+	int uStatus = usb_control_msg(
+		handle->device,
+		USB_ENDPOINT_OUT | USB_TYPE_VENDOR | USB_RECIP_DEVICE,
+		CMD_JTAG_CLOCK,    // bRequest
+		numClocks&0xFFFF,  // wValue
+		numClocks>>16,     // wIndex
+		NULL,
+		0,                 // wLength
+		5000               // timeout (ms)
+	);
+	if ( uStatus < 0 ) {
+		errRender(error, "neroClocks(): %s", usb_strerror());
+		FAIL(NERO_CLOCKS);
+	}
+	return NERO_SUCCESS;
+cleanup:
+	return returnCode;
+}
+
+// -------------------------------------------------------------------------------------------------
+// Implementation of private functions
+// -------------------------------------------------------------------------------------------------
+
+// Kick off a shift operation on the micro. This will be followed by a bunch of sends and receives.
+//
+static NeroStatus beginShift(
+	struct NeroHandle *handle, uint32 numBits, SendType sendType, bool isLast,
+	bool isResponseNeeded, const char **error)
+{
+	NeroStatus returnCode;
+	uint16 wValue = 0x0000;
+	int uStatus;
+	if ( isLast ) {
+		wValue |= (1<<IS_LAST);
+	}
+	if ( isResponseNeeded ) {
+		wValue |= (1<<IS_RESPONSE_NEEDED);
+	}
+	wValue |= sendType << SEND_TYPE;
+	uStatus = usb_control_msg(
+		handle->device,
+		USB_ENDPOINT_OUT | USB_TYPE_VENDOR | USB_RECIP_DEVICE,
+		CMD_JTAG_CLOCK_DATA,  // bRequest
+		wValue,               // wValue
+		0x0000,               // wIndex
+		(char*)&numBits,      // send bit count
+		4,                    // wLength
+		5000                  // timeout (ms)
+	);
+	if ( uStatus < 0 ) {
+		errRender(error, "beginShift(): %s", usb_strerror());
+		FAIL(NERO_BEGIN_SHIFT);
+	}
+	return NERO_SUCCESS;
+cleanup:
+	return returnCode;
+}
+
+// Send a chunk of data to the micro.
+//
+static NeroStatus doSend(
+	struct NeroHandle *handle, const uint8 *sendPtr, uint16 chunkSize, const char **error)
+{
+	NeroStatus returnCode;
+	int uStatus = usb_bulk_write(
+		handle->device,
+		USB_ENDPOINT_OUT | 2,    // write to endpoint 2
+		(char *)sendPtr,         // write from send buffer
+		chunkSize,               // write this many bytes
+		5000                     // timeout in milliseconds
+	);
+	if ( uStatus < 0 ) {
+		errRender(error, "doSend(): %s", usb_strerror());
+		FAIL(NERO_SEND);
+	}
+	return NERO_SUCCESS;
+cleanup:
+	return returnCode;
+}
+
+// Receive a chunk of data from the micro.
+//
+static NeroStatus doReceive(
+	struct NeroHandle *handle, uint8 *receivePtr, uint16 chunkSize, const char **error)
+{
+	NeroStatus returnCode;
+	int uStatus = usb_bulk_read(
+		handle->device,
+		USB_ENDPOINT_IN | 4,    // read from endpoint 4
+		(char *)receivePtr,     // read into the receive buffer
+		chunkSize,              // read this many bytes
+		5000                    // timeout in milliseconds
+	);
+	if ( uStatus < 0 ) {
+		errRender(error, "doReceive(): %s", usb_strerror());
+		FAIL(NERO_RECEIVE);
+	}
+	return NERO_SUCCESS;
+cleanup:
+	return returnCode;
+}
+
+// Find the size of the EP2OUT & EP4IN bulk endpoints (they must be the same)
+//
+static NeroStatus setEndpointSize(struct NeroHandle *handle, const char **error) {
+	NeroStatus returnCode;
+	int uStatus;
+	char descriptorBuffer[1024];  // TODO: Fix by doing two queries
+	char *ptr = descriptorBuffer;
+	uint8 endpointNum;
+	uint16 ep2size = 0;
+	uint16 ep4size = 0;
+	struct usb_config_descriptor *configDesc;
+	struct usb_interface_descriptor *interfaceDesc;
+	struct usb_endpoint_descriptor *endpointDesc;
+	handle->endpointSize = 0;
+	uStatus = usb_control_msg(
+		handle->device,
+		USB_ENDPOINT_IN | USB_TYPE_STANDARD | USB_RECIP_DEVICE,
+		USB_REQ_GET_DESCRIPTOR,    // bRequest
+		0x0200,                    // wValue
+		0x0000,     // wIndex
+		descriptorBuffer,
+		1024,                 // wLength
+		5000               // timeout (ms)
+	);
+	if ( uStatus < 0 ) {
+		errRender(
+			error, "setEndpointSize(): Failed to get config descriptor: %s", usb_strerror());
+		FAIL(NERO_ENDPOINTS);
+	}
+	if ( uStatus > 0 ) {
+		configDesc = (struct usb_config_descriptor *)ptr;
+		ptr += configDesc->bLength;
+		interfaceDesc = (struct usb_interface_descriptor *)ptr;
+		ptr += interfaceDesc->bLength;			
+		endpointNum = interfaceDesc->bNumEndpoints;
+		while ( endpointNum-- ) {
+			endpointDesc = (struct usb_endpoint_descriptor *)ptr;
+			if ( endpointDesc-> bmAttributes == 0x02 ) {
+				if ( endpointDesc->bEndpointAddress == 0x02 ) {
+					ep2size = endpointDesc->wMaxPacketSize;
+				} else if ( endpointDesc->bEndpointAddress == 0x84 ) {
+					ep4size = endpointDesc->wMaxPacketSize;
+				}
+			}
+			ptr += endpointDesc->bLength;
+		}
+	}
+	if ( !ep2size ) {
+		errRender(
+			error, "setEndpointSize(): EP2OUT not found or not configured as a bulk endpoint!");
+		FAIL(NERO_ENDPOINTS);
+	}
+	if ( !ep4size ) {
+		errRender(
+			error, "setEndpointSize(): EP4IN not found or not configured as a bulk endpoint!");
+		FAIL(NERO_ENDPOINTS);
+	}
+	if ( ep2size != ep4size ) {
+		errRender(
+			error, "setEndpointSize(): EP2OUT's wMaxPacketSize differs from that of EP4IN");
+		FAIL(NERO_ENDPOINTS);
+	}
+	handle->endpointSize = ep2size;
+	return NERO_SUCCESS;
+cleanup:
+	return returnCode;
+}
+
+// Put the device in jtag mode (i.e drive or tristate the JTAG lines)
+//
+static NeroStatus setJtagMode(struct NeroHandle *handle, bool enable, const char **error) {
+	NeroStatus returnCode;
+	int uStatus = usb_control_msg(
+		handle->device,
+		USB_ENDPOINT_OUT | USB_TYPE_VENDOR | USB_RECIP_DEVICE,
+		CMD_MODE_STATUS,          // bRequest
+		enable ? MODE_JTAG : 0,   // wValue
+		MODE_JTAG,                // wMask
+		NULL,
+		0,                        // wLength
+		100                       // timeout (ms)
+	);
+	if ( uStatus < 0 ) {
+		errRender(
+			error, "setJtagMode(): Unable to %s JTAG mode: %s",
+			enable ? "enable" : "disable", usb_strerror());
+		FAIL(NERO_ENABLE);
+	}
+	return NERO_SUCCESS;
+cleanup:
+	return returnCode;
+}
---
+++ libnero.h
@@ -0,0 +1,87 @@
+/* 
+ * Copyright (C) 2011 Chris McClelland
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *  
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef LIBNERO_H
+#define LIBNERO_H
+
+#include <makestuff.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	// Possible return codes
+	typedef enum {
+		NERO_SUCCESS,
+		NERO_USB_INIT,
+		NERO_SYNC,
+		NERO_ENDPOINTS,
+		NERO_CLOCKFSM,
+		NERO_CLOCKS,
+		NERO_BEGIN_SHIFT,
+		NERO_SEND,
+		NERO_RECEIVE,
+		NERO_ENABLE
+	} NeroStatus;
+
+	// Return the number of bytes necessary to store x number of bits
+	#define bitsToBytes(x) ((x>>3) + (x&7 ? 1 : 0))
+
+	// Context struct
+	struct usb_dev_handle;
+	struct NeroHandle {
+		struct usb_dev_handle *device;
+		uint16 endpointSize;
+	};
+	
+	// Initialise the connection to the device implementing the NeroJTAG protocol
+	NeroStatus neroInitialise(
+		struct usb_dev_handle *device, struct NeroHandle *handle, const char **error
+	) WARN_UNUSED_RESULT;
+	
+	// Drop the connection to the device implementing the NeroJTAG protocol
+	NeroStatus neroClose(
+		struct NeroHandle *handle, const char **error
+	) WARN_UNUSED_RESULT;
+	
+	// Shift "numBits" bits from "inData" into TDI, at the same time shifting the same number of
+	// bits from TDO into "outData". If "isLast" is true, leave Shift-DR state on final bit. If you
+	// want inData to be all zeros or all ones, you can use ZEROS or ONES respectively. This is more
+	// efficient than physically sending an array containing all zeros or all 0xFFs.
+	NeroStatus neroShift(
+		struct NeroHandle *handle, uint32 numBits, const uint8 *inData, uint8 *outData, bool isLast,
+		const char **error
+	) WARN_UNUSED_RESULT;
+
+	// Special values for inData parameter of neroShift() declared above
+	#define ZEROS (const uint8*)NULL
+	#define ONES (const uint8*)-1
+	
+	// Clock "transitionCount" bits from "bitPattern" into TMS, starting with the LSB.
+	NeroStatus neroClockFSM(
+		struct NeroHandle *handle, uint32 bitPattern, uint8 transitionCount, const char **error
+	) WARN_UNUSED_RESULT;
+	
+	// Toggle TCK "numClocks" times.
+	NeroStatus neroClocks(
+		struct NeroHandle *handle, uint32 numClocks, const char **error
+	) WARN_UNUSED_RESULT;
+	
+#ifdef __cplusplus
+}
+#endif
+
+#endif
